dad2f940c537b9afa9e0b7911145c989ceb758b0
Tri Sutrisno <mas.trisutrisno@gmail.com>
2021-03-28 14:15:01 +0700

    [patch] change tech stats data to progreess bar
    -  get color by percentage data difference

--- src/components/match/sub/BallData.vue
+++ src/components/match/sub/BallData.vue
@@ -6,11 +6,30 @@
         <v-col lg="12">
           <v-row class="d-flex justify-space-between">
             <v-col class="text-left">
-              {{ tech.home }}
+              <v-progress-linear
+                :color="getColorData(tech.home, tech.away)"
+                background-color="grey lighten-3"
+                reverse
+                :value="getPercent(tech.home)"
+                height="20"
+                ><template v-slot:default="{}">
+                  <span style="color: #000"> {{ tech.home }}</span>
+                </template></v-progress-linear
+              >
             </v-col>
-            <v-col class="text-center"> {{ tech.typeName }} </v-col>
+            <v-col class="text-center">
+              {{ tech.typeName.replace(/([a-z])([A-Z])/g, "$1 $2") }}</v-col
+            >
             <v-col class="text-right">
-              {{ tech.away }}
+              <v-progress-linear
+                :color="getColorData(tech.away, tech.home)"
+                background-color="grey lighten-3"
+                :value="getPercent(tech.away)"
+                height="20"
+                ><template v-slot:default="{}">
+                  <span style="color: #000"> {{ tech.away }}</span>
+                </template></v-progress-linear
+              >
             </v-col>
           </v-row>
         </v-col>
@@ -25,6 +44,27 @@ export default {
     tech: {
       type: Object,
       required: true,
+    },
+  },
+  computed: {
+    buffer() {
+      return this.tech.home + this.tech.away;
+    },
+  },
+  methods: {
+    getPercent(val) {
+      return (val / (this.tech.home + this.tech.away)) * 100;
+    },
+    getColorData(val1, val2) {
+      let color;
+      if (val1 > val2) {
+        color = "red darken-2";
+      } else if (val1 < val2) {
+        color = "grey";
+      } else {
+        color = "blue";
+      }
+      return color;
     },
   },
 };
--- src/components/match/sub/BallPossesion.vue
+++ src/components/match/sub/BallPossesion.vue
@@ -3,13 +3,15 @@
     <v-list-item-content>
       <v-row>
         <v-col lg="12">
-          <v-col class="text-center"> {{ tech.typeName }} </v-col>
+          <v-col class="text-center">
+            {{ tech.typeName.replace(/([a-z])([A-Z])/g, "$1 $2") }}
+          </v-col>
         </v-col>
         <v-col cols="12" lg="12" xs="12">
           <v-progress-linear
             rounded
-            background-color="yellow darken-1"
-            color="pink darken-1"
+            :color="getColorData(tech.home, tech.away, 'left')"
+            :background-color="getColorData(tech.away, tech.home, 'right')"
             height="20"
             :value="tech.home"
             ><template v-slot:default="{ value }">
@@ -30,6 +32,19 @@ export default {
     tech: {
       type: Object,
       required: true,
+    },
+  },
+  methods: {
+    getColorData(val1, val2, pos) {
+      let color;
+      if (val1 > val2) {
+        color = "red darken-2";
+      } else if (val1 < val2) {
+        color = "grey";
+      } else {
+        pos === "left" ? (color = "red") : (color = "blue");
+      }
+      return color;
     },
   },
 };
